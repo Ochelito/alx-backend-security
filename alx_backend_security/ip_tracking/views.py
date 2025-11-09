57a1fd68f7c2a36eece48b8aca9b35deb4c6a3e0
Idoko Augustine <linkwithaugustine@gmail.com>
2025-11-09 18:34:33 +0100

    implemented rate limiting

--- alx_backend_security/ip_tracking/views.py
+++ alx_backend_security/ip_tracking/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from ratelimit.decorators import ratelimit
 
-# Create your views here.
+@csrf_exempt
+@ratelimit(key='ip', rate='5/m', block=True)        # for anonymous
+@ratelimit(key='user_or_ip', rate='10/m', block=True)  # for logged-in users
+def login_view(request):
+    if request.method == "POST":
+        return JsonResponse({"message": "Login request accepted"})
+    return JsonResponse({"error": "Use POST"}, status=405)
